d951b66529b828560cc248669148cebb4e450e50
pratikkumar1 <prxtxkarts.private@gmail.com>
2024-09-20 11:23:45 +0530

    merge both pc files

--- basics/test.js
+++ basics/test.js
@@ -1,5 +1,4 @@
 console.log("hello lol");
-=======
 const x = 36423647;
 const y = 28274687;
 
@@ -8,4 +7,3 @@ if (x > y) {
 } else {
   console.log("x is lesser than y");
 }
->>>>>>> 97f7bc87256db64006133f1d2a97a06f54fd0bd9
